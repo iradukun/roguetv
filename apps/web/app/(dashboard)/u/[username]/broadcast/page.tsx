d19729a7b3661b9a2350dcd4d0f132ca505875ef
iradukun <mustaphairadukunda@gmail.com>
2024-04-15 21:41:59 +0200

    fix(web): fxed broadcast page ui

--- apps/web/app/(dashboard)/u/[username]/broadcast/page.tsx
+++ apps/web/app/(dashboard)/u/[username]/broadcast/page.tsx
@@ -15,11 +15,11 @@ export default async function BroadcastPage({ params }: BroadcastPageProps) {
   const broadcasts = await getBroadcasts() as Broadcast[];
 
   return (
-    <div className="p-6">
+    <div className="p-6 w-full">
       <div className="mb-4">
         <h1 className="text-2xl font-bold">Past broadcasts</h1>
       </div>
-      <div className="grid grid-cols-4">
+      <div className="grid grid-cols-3 w-full gap-8">
         {broadcasts.map((broadcast) => (<BroadcastCard key={broadcast.id} {...broadcast} />))}
       </div>
     </div>
